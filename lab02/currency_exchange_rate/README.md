a662e4cca11e1f4718eb1602a3141b88c163b7a8
slendchat <slendchat@gmail.com>
2025-09-28 12:47:41 +0000

    started working on parser

---
+++ lab02/currency_exchange_rate/README.md
@@ -0,0 +1,7 @@
+to run
+```bash
+# one date
+python3 myapp USD EUR 2025-01-01 --api-url http://localhost:8080/api --api-key TEST_KEY
+# in a range of dates
+python3 myapp USD EUR --range START END --api-url http://localhost:8080/api --api-key TEST_KEY
+```
--- lab02/currency_exchange_rate/src/currency_exchange_rate/cli.py
+++ lab02/currency_exchange_rate/src/currency_exchange_rate/cli.py
@@ -1,13 +1,30 @@
 import logging
+import argparse
+import datetime as dt
 
 from . import logger_setup
 from . import api
 
+def check_currency(currency: str) -> str:
+    pass
+
+def check_date(date_str: str) -> dt.date:
+    pass
+
+def build_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    
+    return parser
+
+def validate_args(args: argparse.Namespace) -> None:
+    pass
+
 def main() -> int:
     logger_setup.init_logger()
     log = logging.getLogger("app.cli")
-    log.error("logger test from cli")
-    api.testing_api()
+    args = build_parser().parse_args()
+
+
     return 0
 
 if __name__ == "__main__":
