be6739b0544dfaa6f370c2189fce6dcb86d94b95
slendchat <slendchat@gmail.com>
2025-09-28 21:49:24 +0000

    args

--- lab02/currency_exchange_rate/src/currency_exchange_rate/cli.py
+++ lab02/currency_exchange_rate/src/currency_exchange_rate/cli.py
@@ -6,13 +6,23 @@ from . import logger_setup
 from . import api
 
 def check_currency(currency: str) -> str:
-    pass
 
-def check_date(date_str: str) -> dt.date:
-    pass
+    return currency
+
+def iso_date(date_str: str) -> dt.date:
+    return dt.datetime.strptime(date_str, "%Y-%m-%d").date()
 
 def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
+    parser.add_argument("base", type=check_currency, help="Base currency")
+    parser.add_argument("quote", type=check_currency, help="Quote currency")
+    parser.add_argument("date",nargs="?",type=iso_date,metavar="DATE", help="Date in format YYYY-MM-DD")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--range", nargs=2, metavar=("START","END"), type=iso_date, help="Date range in format YYYY-MM-DD YYYY-MM-DD")
+
+    parser.add_argument("--api-url", default="https://api.exchangerate.host", help="Exchange rate API URL")
+    parser.add_argument("--api-key", default=None, help="Exchange rate API key")
     
     return parser
 
