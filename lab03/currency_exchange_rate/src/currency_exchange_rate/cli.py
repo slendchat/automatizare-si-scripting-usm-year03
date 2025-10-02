c29692bf64db55aa68436dd155b1ce10367aa5ad
Slendchat <slendchat@gmail.com>
2025-10-02 17:15:50 +0300

    it is working but need to fix cron

--- lab03/currency_exchange_rate/src/currency_exchange_rate/cli.py
+++ lab03/currency_exchange_rate/src/currency_exchange_rate/cli.py
@@ -1,17 +1,17 @@
 import logging
 import argparse
 import datetime as dt
+from pathlib import Path
 
 from . import logger_setup
 from . import api
 from . import utils
 
-logger_setup.init_logger()
 log = logging.getLogger("app.cli")
 
 def check_currency(currency: str) -> str:
     allowed_currencies = ["USD","EUR","RUB","RON","UAH"]
-    if not (currency in allowed_currencies):
+    if currency not in allowed_currencies:
         log.error(f"Bad currency: {currency}")
         raise argparse.ArgumentTypeError("Bad currency")
     return currency
@@ -19,7 +19,7 @@ def check_currency(currency: str) -> str:
 def iso_date(date_str: str) -> dt.date:
     try:
         return dt.datetime.strptime(date_str, "%Y-%m-%d").date()
-    except:
+    except Exception:
         log.error("Bad date")
         raise argparse.ArgumentTypeError("Bad date")
 
@@ -27,14 +27,20 @@ def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument("base", type=check_currency, help="Base currency")
     parser.add_argument("quote", type=check_currency, help="Quote currency")
-    parser.add_argument("date",nargs="?",type=iso_date,metavar="DATE", help="Date in format YYYY-MM-DD")
+    parser.add_argument("date", nargs="?", type=iso_date, metavar="DATE", help="Date in format YYYY-MM-DD")
 
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("--range", nargs=2, metavar=("START","END"), type=iso_date, help="Date range in format YYYY-MM-DD YYYY-MM-DD")
+    group.add_argument("--range", nargs=2, metavar=("START", "END"), type=iso_date, help="Date range in format YYYY-MM-DD YYYY-MM-DD")
 
     parser.add_argument("--api-url", required=True, default="https://api.exchangerate.host", help="Exchange rate API URL")
     parser.add_argument("--api-key", required=True, default=None, help="Exchange rate API key")
-    
+
+    # новые параметры
+    parser.add_argument("--data-dir", default=str(Path(__file__).resolve().parents[2] / "data"),
+                        help="Directory to save responses (default: ./data)")
+    parser.add_argument("--log-dir", default=str(Path(__file__).resolve().parents[2] / "logs/app.log"),
+                        help="Path to log file (default: ./logs/app.log)")
+
     return parser
 
 def validate_args(args: argparse.Namespace):
@@ -44,49 +50,54 @@ def validate_args(args: argparse.Namespace):
     elif args.date is not None and args.range is not None:
         log.error("date and date range are given both")
         raise SystemExit("date and date range are given both")
-    
+
     if args.range is not None:
         start, end = args.range
         if start >= end:
             log.error(f"starting date {start} is bigger or equal to end date {end}")
             raise SystemExit(f"starting date {start} is bigger or equal to end date {end}")
-        
+
 def date_range(start: dt.date, end: dt.date):
     cur = start
     one = dt.timedelta(days=1)
     while cur <= end:
         yield cur
         cur += one
-        
+
 def main() -> int:
     args = build_parser().parse_args()
+    Path(args.data_dir).mkdir(parents=True, exist_ok=True)
+    Path(args.log_dir).parent.mkdir(parents=True, exist_ok=True)
+
+    logger_setup.init_logger(log_path=args.log_dir)
+
     validate_args(args)
+
     if args.range is not None:
         responses = []
         start, end = args.range
         for date_i in date_range(start, end):
             responses.append(
                 api.fetch_exchange_rate_to_json(
-                args.api_url, 
-                args.api_key, 
-                args.base, 
-                args.quote, 
-                date_i.isoformat()
+                    args.api_url,
+                    args.api_key,
+                    args.base,
+                    args.quote,
+                    date_i.isoformat()
                 )
-            ) 
-        utils.save_multiple_responses_in_file(responses,args)
+            )
+        utils.save_multiple_responses_in_file(responses, args)
     else:
         utils.save_response_in_file(
             api.fetch_exchange_rate_to_json(
-                args.api_url, 
-                args.api_key, 
-                args.base, 
-                args.quote, 
+                args.api_url,
+                args.api_key,
+                args.base,
+                args.quote,
                 args.date.isoformat()
             ),
             args
         )
-
 
     return 0
 
--- lab03/currency_exchange_rate/src/currency_exchange_rate/logger_setup.py
+++ lab03/currency_exchange_rate/src/currency_exchange_rate/logger_setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def get_file_root() -> Path:
     return Path(__file__).resolve().parents[2]
 
-def init_logger(logger_name: str = "app") -> Logger:
+def init_logger(log_path: str | Path | None = None, logger_name: str = "app") -> Logger:
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
     logger.propagate = False
@@ -13,17 +13,25 @@ def init_logger(logger_name: str = "app") -> Logger:
     if logger.handlers:
         return logger
 
+    # форматы логов
     file_fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     console_fmt = logging.Formatter("%(asctime)s %(levelname)s %(name)s %(filename)s:%(lineno)d: %(message)s")
 
+    # вывод в консоль
     logger_console = logging.StreamHandler()
     logger_console.setFormatter(console_fmt)
+    logger.addHandler(logger_console)
 
-    log_path = get_file_root() / 'currency_exchange_rate.log'
-    logger_file = logging.FileHandler(log_path, mode='a', encoding='utf-8')
+    # лог в файл
+    if log_path is None:
+        log_path = get_file_root() / "currency_exchange_rate.log"
+
+    log_path = Path(log_path).resolve()
+    log_path.parent.mkdir(parents=True, exist_ok=True)
+
+    logger_file = logging.FileHandler(log_path, mode="a", encoding="utf-8")
     logger_file.setFormatter(file_fmt)
     logger_file.setLevel(logging.ERROR)
-
-    logger.addHandler(logger_console)
     logger.addHandler(logger_file)
+
     return logger
