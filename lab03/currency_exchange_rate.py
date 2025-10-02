2bc85f736ba0f6b7438cf65809c4182c8750d7db
Slendchat <slendchat@gmail.com>
2025-10-02 15:13:12 +0300

    gdsfgdfgdf

--- lab03/currency_exchange_rate.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Thin wrapper so cron can invoke the lab02 package."""
-from currency_exchange_rate.cli import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
